2a70338256ed5a28ada6ddf427876628ad908828
jomarierafa <jomarierafa1717@gmail.com>
2025-09-02 16:46:50 +0800

    feat: Add HistoryItem and HistoryItemMenu composables
    This commit introduces two new composables: `HistoryItem` and `HistoryItemMenu`.
    
    - `HistoryItem` is a composable function that displays a single item in the history list. It includes an icon, title, content, and date/time.
    - `HistoryItemMenu` is a modal bottom sheet that provides options for sharing and deleting a history item.
    
    Additionally, this commit includes:
    - A new trash icon (`ic_trash.xml`)
    - A new string resource for "Delete"

---
+++ app/src/main/java/com/jvrcoding/qrcraft/qr/presentation/history/components/HistoryItem.kt
@@ -0,0 +1,84 @@
+package com.jvrcoding.qrcraft.qr.presentation.history.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jvrcoding.qrcraft.R
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.OnSurfaceAlt
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.OnSurfaceDisabled
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.QRCraftTheme
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.Text
+
+@Composable
+fun HistoryItem(
+    iconRes: Int,
+    iconTint: Color,
+    title: String,
+    content: String,
+    dateTime: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            tint = iconTint,
+            contentDescription = null,
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = content,
+                style = MaterialTheme.typography.bodyMedium,
+                color = OnSurfaceAlt,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = dateTime,
+                style = MaterialTheme.typography.bodySmall,
+                color = OnSurfaceDisabled
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun HistoryItemPreview() {
+    QRCraftTheme {
+        HistoryItem(
+            iconRes = R.drawable.ic_text,
+            iconTint = Text,
+            title = "Text",
+            content = "Adipiscing ipsum lacinia tincidunt sed. In risus dui accumsan accumsan quam morbi nulla. Dictum justo metus auctor nunc quam id sed. Urna nisi gravida sed lobortis diam pretium.",
+            dateTime = "24 Jun 2025, 14:36"
+        )
+    }
+}
---
+++ app/src/main/java/com/jvrcoding/qrcraft/qr/presentation/history/components/HistoryItemMenu.kt
@@ -0,0 +1,95 @@
+package com.jvrcoding.qrcraft.qr.presentation.history.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jvrcoding.qrcraft.R
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.QRCraftTheme
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.ShareIcon
+import com.jvrcoding.qrcraft.core.presentation.designsystem.theme.TrashIcon
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HistoryItemMenu(
+    onShareClick: () -> Unit,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        modifier = modifier,
+        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        containerColor = MaterialTheme.colorScheme.surface,
+        sheetMaxWidth = 412.dp,
+        dragHandle = {}
+    ) {
+        TextButton(
+            onClick = onShareClick,
+            shape = RectangleShape,
+            colors = ButtonDefaults.outlinedButtonColors(
+                contentColor = MaterialTheme.colorScheme.onSurface,
+            ),
+            border = null,
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = ShareIcon,
+                contentDescription = stringResource(R.string.share),
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(R.string.share),
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+        TextButton(
+            onClick = onShareClick,
+            shape = RectangleShape,
+            colors = ButtonDefaults.outlinedButtonColors(
+                contentColor = MaterialTheme.colorScheme.error,
+            ),
+            border = null,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = TrashIcon,
+                contentDescription = stringResource(R.string.delete),
+                tint = MaterialTheme.colorScheme.error
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(R.string.delete),
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun HistoryItemMenuPreview() {
+    QRCraftTheme {
+        HistoryItemMenu(
+            onShareClick = {},
+            onDismiss = {}
+        )
+    }
+}
